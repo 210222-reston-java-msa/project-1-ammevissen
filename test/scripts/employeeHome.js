9caa31c9508deb9f8593f89e473ec82f301e2415
ammevissen <mevissenjobs@gmail.com>
2021-03-11 16:37:14 -0600

    renaming project

--- test/scripts/employeeHome.js
+++
@@ -1,9 +0,0 @@
-function task(){
-    console.log("task choosen")
-    let task=document.getElementById("task").value
-    console.log(task)
-}
-
-var choice = document.getElementById("submit");
-
-choice.addEventListener("click", task);
